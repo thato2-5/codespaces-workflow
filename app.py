49fb3341651dad79d89be96a136667b7f7ebb9db
Thato Monyamane <62892510+thato2-5@users.noreply.github.com>
2024-08-22 10:08:14 +0200

    second attempt to test for app service deployment after removing the python virtual environment @ 8/22/24

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def download(upload_id):
 if __name__ == "__main__":
 	with app.app_context():
 		db.create_all()
-	app.run(debug = True, host = '0.0.0.0')
+	app.run(debug = True, host = '0.0.0.0')		# Use available ip address
