a843613320d72aaaaea8b14bbe622249417874a6
Thato Monyamane <62892510+thato2-5@users.noreply.github.com>
2024-08-27 13:14:16 +0200

    Added new app functionality to test the deployment when using azure app service

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'thatomonyamane3@gmail.com'
-app.config['MAIL_PASSWORD'] = '90059Jay#'
+app.config['MAIL_USERNAME'] = 'os.environ.get('MAIL_USERNAME')'
+app.config['MAIL_PASSWORD'] = 'os.environ.get('MAIL_PASSWORD')'
 app.config['MAIL_DEFAULT_SENDER'] = ('Thato Monyamane', 'thatomonyamane3@gmail.com')
 
 mail = Mail(app)
