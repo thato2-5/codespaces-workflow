cc1572f3ca00946bfa0de0ab7c66086b3734138b
Thato Monyamane <62892510+thato2-5@users.noreply.github.com>
2024-09-02 08:57:21 +0000

    Added navigational function for different templates

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('maintenance.html')
 
+@app.route('/test')
+def test():
+    return render_template('comingsoon.html')
+
+@app.route('/check')
+def check():
+    return render_template('construction.html')
+
+@app.route('/maintain')
+def maintain():
+    return render_template('maintenance01.html')
+
 if __name__ == '__main__':
     app.run(debug = True, host ='0.0.0.0')
     
