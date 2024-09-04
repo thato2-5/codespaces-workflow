569fbe87844cca02dd036854d0c726b0b9449848
Thato Monyamane <62892510+thato2-5@users.noreply.github.com>
2024-09-04 10:36:39 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('maintenance.html')
+    return render_template('maintenance01.html')
 
 @app.route('/test')
 def test():
